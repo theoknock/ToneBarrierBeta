5a96786aba84772ac8a0a7f695c0e1f30fcb57d3
James Alan Bush <jamesalanbush1972@gmail.com>
2022-06-15 22:45:04 -0400

    Initial Commit

---
+++ ToneBarrier/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ToneBarrier
+//
+//  Created by Xcode Developer on 6/15/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ ToneBarrier/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  ToneBarrier
+//
+//  Created by Xcode Developer on 6/15/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
